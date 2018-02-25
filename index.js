ffd3597b2769d80692fb71440d4f31d50f72112e
Sarah Currey <curreysarah28@gmail.com>
2018-02-25 15:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function shout(string) {
-  console.log()
+  console.log('YES INDEED!')
 return string.toUpperCase()
   }
 function whisper(string) {
+  console.log ('I cannot hear you')
 return string.toLowerCase()
 }
 function sayHelloToGrandma() {

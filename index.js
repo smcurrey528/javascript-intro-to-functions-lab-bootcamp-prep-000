92bc007fdbcdc4fa193ccb7cd0d9cab417b70ac2
Sarah Currey <curreysarah28@gmail.com>
2018-02-25 16:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ function whisper(string) {
 return string.toLowerCase()
 }
 
-
-function sayHitoGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can't hear you!";
-  }
-  if (string === string.toUpperCase()) {
-    return "YES INDEED!";
-  }
-  if (string === "I love you, Grandma.") {
-    return "I love you, too.";
-  }
+function sayHiToGrandma(string){
+  return "I can't hear you!"
 }
+function sayHiToGrandma(string) {
+ if (string.toLowerCase() === string) {
+  return "I can't hear you!"
+} else if (string.toUpperCase() === string)
+return "YES INDEED!" }
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you too."
+  } else if (string === ) }
+
 function logShout(string){
   console.log(string.toUpperCase())
   }
